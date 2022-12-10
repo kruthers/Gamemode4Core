9f7fac82fa726d7a61b05338ce3ab807a58623cc
kruthers <pandakruthers@gmail.com>
2022-12-10 12:14:02 +0000

    Fix streamer mode not saving state

--- src/main/kotlin/com/kruthers/gamemode4core/modes/StreamerMode.kt
+++ src/main/kotlin/com/kruthers/gamemode4core/modes/StreamerMode.kt
@@ -13,16 +13,14 @@ class StreamerMode {
     companion object {
 
         fun toggle(plugin: Gamemode4Core, player: Player) {
-            var playerData: YamlConfiguration = loadPlayerData(plugin, player)
+            val playerData: YamlConfiguration = loadPlayerData(plugin, player)
+            val inMode: Boolean = playerData.getBoolean("mode.streamer")
 
-            playerData = if (playerData.getBoolean("mode.streamer")) {
+           if (inMode) {
                 disable(plugin, player, playerData)
             } else {
                 enable(plugin, player, playerData)
             }
-
-            playerData.save(getPlayerDataFile(plugin, player))
-
         }
 
 
@@ -39,8 +37,9 @@ class StreamerMode {
                             "to change mode. If this continues please inform you system admin", NamedTextColor.RED))
                     plugin.logger.warning("Failed to put ${player.name} into streamer mode: Failed to find group: $group")
                 }
-            })
 
+                playerData.save(getPlayerDataFile(plugin, player))
+            })
 
             return playerData
         }
@@ -59,6 +58,8 @@ class StreamerMode {
                             "(You are still in streamer mode)", NamedTextColor.RED))
                     plugin.logger.warning("Failed to exit ${player.name} from streamer mode: Failed to find group: $group")
                 }
+
+                playerData.save(getPlayerDataFile(plugin, player))
             })
 
             return playerData
