7de480fa1d16816ba9fb1b8c00870b4e554f619a
EpyonProjects <theepyonproject@gmail.com>
2024-02-18 00:26:17 -0500

    Removed additional scheduled task - removed check using map.values because it is also O(n), so it accomplishes the same by only doing the for each and just skipping with return@forEach

--- src/main/kotlin/com/kruthers/gamemode4core/events/DimensionEvents.kt
+++ src/main/kotlin/com/kruthers/gamemode4core/events/DimensionEvents.kt
@@ -8,42 +8,36 @@ import org.bukkit.Bukkit
 import org.bukkit.World
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.EntityPortalEvent
 import org.bukkit.event.player.PlayerTeleportEvent
 
 class DimensionEvents(val plugin: Gamemode4Core): Listener {
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     fun onDimensionChange(event: PlayerTeleportEvent) {
         if (event.from.world != event.to.world) {
             val player: Player = event.player
 
-            //if the player is being watched, inform the watcher
-            if (Gamemode4Core.watchingPlayers.values.contains(player.uniqueId)) {
-                Gamemode4Core.watchingPlayers.forEach { watcher, target ->
-                    if (target == player.uniqueId) {
-                        watcher.spectatorTarget?.let {
-                            watcher.spectatorTarget = null
-                            Bukkit.getScheduler().runTaskLater(plugin, Runnable {
-                                watcher.teleport(event.to)
-                                Bukkit.getScheduler().runTaskLater(plugin, Runnable {
-                                    watcher.spectatorTarget = player
-                                }, 4L)
-                            }, 1L)
-                        }
-
-
-                        watcher.sendMessage(getMessage(plugin,"watch.dimension_change",watcher,
-                            TagResolver.resolver(Placeholder.unparsed("target",player.name))))
-                    }
+            // if the player is being watched, inform the watcher
+            Gamemode4Core.watchingPlayers.forEach { (watcher, target) ->
+                if (target != player.uniqueId) { return@forEach }
+                watcher.spectatorTarget?.let {
+                    watcher.spectatorTarget = null
+                    watcher.teleport(event.to)
+                    Bukkit.getScheduler().runTaskLater(plugin, Runnable {
+                        watcher.spectatorTarget = player
+                    }, 4L)
                 }
-            }
 
+                watcher.sendMessage(getMessage(plugin,"watch.dimension_change",watcher,
+                    TagResolver.resolver(Placeholder.unparsed("target",player.name))))
+            }
         }
     }
 
-    //Item Teleportation
+    // Item Teleportation
     @EventHandler
     fun onEntityDimensionChange(event: EntityPortalEvent) {
         if (event.from.world?.environment == World.Environment.THE_END) {
